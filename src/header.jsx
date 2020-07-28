44aeeb69daf1d3f889238e3738985a942d019370
Joseph Yeh <josephycyeh@gmail.com>
2020-07-28 16:06:12 -0700

    yes

--- src/header.jsx
+++ src/header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-
+import sync from './sync.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
     return ( 
         <header>
         <Navbar expand="lg" className='.d-none .d-sm-block' >
-        <Navbar.Brand href="#home"><h1 className='logo'>Sync.</h1></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={sync} width='60' height='60' className="d-inline-block align-top"></img> <h1 className='logo'>Sync.</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" style={{visibility:'hidden'}}/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#programs"><h2 className='nav-item'>Fellowship</h2></Nav.Link>
-            <Nav.Link href="#partners"><h2 className='nav-item'>Partners</h2></Nav.Link>
-            <Nav.Link href="https://josephycyeh.typeform.com/to/Pugk9J8T"><h2 className='nav-item'>Apply</h2></Nav.Link>
+            <Nav.Link href="#programs"><h1 className='nav-item'>Fellowship</h1></Nav.Link>
+            <Nav.Link href="#partners"><h1 className='nav-item'>Partners</h1></Nav.Link>
+            <Nav.Link href="https://josephycyeh.typeform.com/to/Pugk9J8T"><h1 className='nav-item'>Apply</h1></Nav.Link>
         
           </Nav>
           
