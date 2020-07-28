6e67ae98f0cfe67ab18b1c1cc5e2640db0f11b93
Joseph Yeh <josephycyeh@gmail.com>
2020-07-28 04:01:19 -0700

    layout

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -29,18 +29,18 @@ function Detail() {
             <h2 style={{textAlign:'center', color:'black'}}>What We Do</h2>   
             <Row>
             
-                <Col>
+                <Col md='6' sm='12'>
                 <i className='ion-university'></i>
                 <h5>Mentorship</h5>
                 <p>We help you build the necessary skills and provide you with the tools you need to succeed as a founder</p>
                 </Col>
-                <Col>
+                <Col md='6' sm='12'>
                 <i className='ion-cash'></i>
                 <h5>Funding</h5>
                 <p>We provide funding to a selected number of startups and help you manage the money and resources</p>
                 </Col>
                 
-                <Col>
+                <Col md='6' sm='12'>
                 <i className='ion-person-stalker'>
             
                 </i>
@@ -53,12 +53,12 @@ function Detail() {
             <section className='program-row' id='programs'>
             <h2 style={{textAlign:'center', color:'black'}}>Our Programs</h2>
             <Row>
-                <Col>
+                <Col md='6' sm='12'>
                     <i className='ion-code-working'></i>
                     <h5>Startup Program</h5>
                     <p>Our startup program is a 2 month long fellowship designed to get you from an idea to an MVP (minimum viable product) and attract an user base.</p>
                 </Col>
-                <Col>
+                <Col md='6' sm='12'>
                     <i className='ion-stats-bars'></i>
                     <h5>Executive Program</h5>
                     <p>Our executive program is a 2 week long fellowship designed to get you from up and running to help us facilitate and manage the club.</p>
@@ -73,27 +73,27 @@ function Detail() {
             <section className='partner-row' id='partners'>
             <h2>Our Partners</h2>
                 <Row>
-                    <Col>
+                    <Col md='6' sm='12'>
                        <img src={helena} alt='hello' className='profile' />
                        <h5>Helena Merk</h5>
                        <p>Co-founder and CEO of Glimpse (YC W20)</p>
                     </Col>
-                    <Col>
+                    <Col md='6' sm='12'>
                         <img src={john} alt='hello' className='profile'></img>
                         <h5>John Xie</h5>
                         <p>Co-founder and CEO of Taskade (YC W19)</p>
                     </Col>
-                    <Col>
+                    <Col md='6' sm='12'>
                         <img src={arjun} alt='hello' className='profile'></img>
                         <h5>Arjun Vairavan</h5>
                         <p>Co-founder and CEO of Kama (Columbia)</p>
                     </Col>
-                    <Col>
+                    <Col md='6' sm='12'>
                        <img src={melissa} alt='hello' className='profile' />
                        <h5>Melissa Du</h5>
                        <p>Co-founder and CEO of Cadence (YC W20)</p>
                     </Col>
-                    <Col>
+                    <Col md='6' sm='12'>
                        <img src={malia} alt='hello' className='profile' />
                        <h5>Malia Jiang</h5>
                        <p>Co-founder and COO of Spark (Berkeley)</p>
@@ -103,11 +103,11 @@ function Detail() {
             </section>
             <section>
                 <Row className='explain-process-row'>
-                    <Col className='explanation-column'>
+                    <Col className='explanation-column' md='6' sm='12'>
                         <h1>How it works</h1>
                         
                     </Col>
-                    <Col className='explanation-column'>
+                    <Col className='explanation-column' md='6' sm='12'>
                         <div className='explanation-image'>
                             <p>Every semester, we select a few startups to be in our fellowship program and we help them build their product. At the end of the fellowship, we will pick a few startups to invest in. 
                             <br></br>We provide our members with the necessary tools and resources to succeed, so they can focus on building good products.</p>
